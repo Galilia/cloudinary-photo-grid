a880d5c264d11bce94d5981747e0183a9ca4a2ae
Ilia <Galilia@users.noreply.github.com>
2022-02-19 13:59:15 +0200

    added key

--- src/components/rightSide/tagsCardGrid/TaggedPhotosItem.jsx
+++ src/components/rightSide/tagsCardGrid/TaggedPhotosItem.jsx
@@ -12,7 +12,7 @@ const TaggedPhotosItem = ({tag}) => {
         <li className="collection-header" style={{ backgroundColor: `#${tag.color}`}}><h5>Tag {tag.header}</h5></li>
         <li className="collection-item">
             {tag.photo.map(({imgId, imgUrl}, index) => (
-              <div className="tagged-photos-item-photoInfo">
+              <div className="tagged-photos-item-photoInfo" key={index}>
                 <img className="tagged-photos-item-img" src={imgUrl} />
                 <div className="right">Photo {imgId}</div>
                 <i className="material-icons btn-pointer" onClick={() => untagPhoto({id, imgId})}>delete</i>
