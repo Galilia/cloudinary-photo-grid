5588fc1020a05ebef4558ff9dc96921bff77f99e
Ilia <Galilia@users.noreply.github.com>
2022-02-19 13:36:25 +0200

    added ref to dropdown

--- src/components/rightSide/photosCardGrid/PhotoItem.jsx
+++ src/components/rightSide/photosCardGrid/PhotoItem.jsx
@@ -1,5 +1,5 @@
 import {Dropdown} from "../../dropdown/Dropdown";
-import {useContext, useState} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 import {TagContext} from "../../../context/context";
 import "./PhotoItem.scss";
 import {Announce} from "../../announce/Announce";
@@ -8,10 +8,28 @@ const PhotosItem = ({img}) => {
   const {tags = []} = useContext(TagContext);
   const [isListOpen, setListOpen] = useState(false);
   const {assignTagToPhoto} = useContext(TagContext);
+  const ref = useRef(null);
 
   const toggleList = () => {
     setListOpen((prevState) => !prevState);
   };
+
+  const handleMouseOut = () => {
+    setTimeout(() => {
+      setListOpen(false);
+    }, 1000);
+  }
+
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener("mouseout", handleMouseOut);
+
+      return () => {
+        node.removeEventListener("mouseout", handleMouseOut);
+      };
+    }
+  }, [ref.current]);
 
   return (
       <div className="card">
@@ -19,10 +37,10 @@ const PhotosItem = ({img}) => {
           <img className="activator" src={img.imgUrl} />
         </div>
         <div className="card-content">
-          <span className="card-title grey-text text-darken-4">Photo {img.imgId}
+          <span className="card-title grey-text text-darken-4" >Photo {img.imgId}
             <button className="material-icons btn right" onClick={toggleList}>more_vert</button>
             {isListOpen &&
-              <div role="list" className="photo-item-dropdown-list">
+              <div role="list" className="photo-item-dropdown-list" ref={ref}>
                 {
                   tags.length ?
                     <>
